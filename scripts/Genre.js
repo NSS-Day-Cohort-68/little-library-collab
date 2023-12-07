a546ce53d23f9943699668e328f6796f76f5fa82
Greg Korte <gregmkorte@gmail.com>
2023-12-07 13:27:31 -0600

    Adding get single genre functionality

--- scripts/Genre.js
+++ scripts/Genre.js
@@ -2,4 +2,21 @@
 //       - Functions:
 //         - Export `getGenre(genreId)` Use the genreId to return the appropriate genre
 //              - Set a variable allGenres to get all Genres by using getGenres from the database
+
 //                     - Iterate allGenres and return the appropriate genre type
+import { getGenres } from "../database.js";
+
+export const getGenre = (genreId) => {
+  const allGenres = getGenres();
+
+  const genreObj = allGenres.find((genre) => genre.id === genreId);
+
+  // This does the same thing as .find()
+  // for (const genre of allGenres){
+  //     if(genre.id === genreId) {
+  //         return genre
+  //     }
+  // }
+
+  return genreObj.type;
+};
