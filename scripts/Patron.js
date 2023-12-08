d7bba12a578dbe15648496984f09d1fba200d03b
Greg Korte <gregmkorte@gmail.com>
2023-12-08 13:09:55 -0600

    Adding get single patron functionality

--- scripts/Patron.js
+++ scripts/Patron.js
@@ -4,3 +4,13 @@
 //         - Export `getPatron(patronId)` Use the patronId to return the appropriate patron
 //              - Create variable allPatrons using `getPatrons` from the database
 //              - Iterate the allPatrons array use the (.find()) array method and return the appropriate patron object
+
+import { getPatrons } from "../database.js";
+
+export const getPatron = (patronId) => {
+  const allPatrons = getPatrons();
+
+  const patronObj = allPatrons.find((patron) => patron.id === patronId);
+
+  return patronObj;
+};
