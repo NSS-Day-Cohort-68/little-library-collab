1c4bb194be6e2c7eb8ef15202e39fb15be275dcf
Greg Korte <gregmkorte@gmail.com>
2023-12-11 12:26:10 -0600

    Adding functionality to get one book by id

--- scripts/Book.js
+++ scripts/Book.js
@@ -7,3 +7,25 @@
 //           - Need bookLog from `BookLog.js`
 //             - Use the bookLog to determine if the book is in stock
 //         - Export `getBook(bookId)` Use the bookId to return the appropriate book
+
+// import { getBooks } from "../database.js";
+
+// export const bookList = () => {
+//   return;
+// };
+
+// Get One book
+// import `getBooks` from `Database.js`
+// export function getBook(bookId)
+// Get all books from database set it to variable named allBooks
+// Use .find() method & bookId to find the appropriate book
+// Return book obj
+
+import { getBooks } from "../database.js";
+
+export const getBook = (bookId) => {
+  const allBooks = getBooks();
+
+  const bookObj = allBooks.find((book) => book.id === bookId);
+  return bookObj;
+};
